1cbe4be8306f60fdc8eb033b86988598ef060cb6
Tinniam V Ganesh <tvganesh.85@gmail.com>
2016-03-27 08:25:47 +0530

    Changes

--- R/getBowlerWicketDetails.R
+++ R/getBowlerWicketDetails.R
@@ -5,20 +5,63 @@
 # This function gets the bowling details of a bowler
 #
 ###########################################################################################
+#' @title
+#' Get the bowling details of a bowler of a team
+#'
+#' @description
+#' This function gets the bowling of a bowler (overs,maidens,runs,wickets,venue, opposition)
+#'
+#' @usage
+#' getBowlerWicketDetails(team,name)
+#'
+#' @param team
+#' The tam to which the bowler belongs
+#'
+#' @param theTeam
+#' The name of the bowler
+#'
+#' @return dataframe
+#' The dataframe of bowling performance
+#'
+#' @references
+#' \url{http://cricsheet.org/}\cr
+#' \url{https://gigadom.wordpress.com/}
+#' @author
+#' Tinniam V Ganesh
+#' @note
+#' Maintainer: Tinniam V Ganesh \email{tvganesh.85@gmail.com}
+#'
+#' @examples
+#' # Get the bowling details of bowlers of a team e.g. India. This is saved as a dataframe
+#' c <- getTeamBowlingDetails("India",dir="../data",save=TRUE)
+#' #Get the bowler details from this overall data frame
+#' jadeja <- getBowlerWicketDetails(team="India",name="Jadeja")
+#'
+#' # The dataframe from the above call is used in many functions
+#' #bowlerMeanEconomyRate(jadeja,"RA Jadeja")
+#'
+#' @seealso
+#' \code{\link{bowlerMovingAverage}}
+#' \code{\link{getTeamBowlingDetails}}
+#' \code{\link{bowlerMeanRunsConceded}}
+#' \code{\link{teamBowlerWicketRunsOppnAllMatches}}
+#'
+#' @export
+#'
 getBowlerWicketDetails <- function(team,name){
     fl <- paste("./",team,"-BowlingDetails.RData",sep="")
     load(fl)
     details <- bowlingDetails
     bowlerDetails <- filter(details,grepl(name,bowler))
     bowlerDetails <- arrange(bowlerDetails,date)
-    
+
     # Count wickets taken
     # Replace nobody by 0 wickets
     a <- filter(bowlerDetails,wicketPlayerOut == "nobody")
     a$wickets <- 0
     a1 <- c <- select(a,bowler,overs,maidens,runs,economyRate,date,
                       opposition,wickets,venue)
-    
+
     # Get rows which have wickets
     b <- filter(bowlerDetails,wicketPlayerOut != "nobody")
     c <- select(b,bowler,overs,maidens,runs,economyRate,date,opposition,venue)
@@ -26,11 +69,11 @@ getBowlerWicketDetails <- function(team,name){
     d <- summarise(group_by(b,date),wickets=length(unique(wicketPlayerOut)))
     # Join tables
     e <- full_join(c,d,by="date")
-    
+
     f <- rbind(a1,e)
     f <- select(f,bowler,overs,maidens,runs,wickets,economyRate,date,opposition,venue)
     g <- arrange(f,date)
     g
-    
-    
+
+
 }
--- R/getDeliveryWickets.R
+++ R/getDeliveryWickets.R
@@ -5,10 +5,55 @@
 # This function creates a data frame of delivery and wickets
 #
 ###########################################################################################
+#' @title
+#' Get and create a data frame of deliveries bowled and wickets taken
+#'
+#' @description
+#' This function  creates a data frame of deliveries bowled and wickets taken. This data frame is
+#' then used by bowlerWktsPredict to predict the number of deliveries to wickets taken
+#'
+#' @usage
+#' getDeliveryWickets(team,dir=".",name,save=FALSE)
+#'
+#' @param dir
+#' The source directory in which the match .RData files exist
+#'
+#' @param name
+#' The name of the bowler
+#'
+#' @param save
+#' Whether the data frame needs to be saved to a file or nor
+#'
+#' @return dataframe
+#' The dataframe of delivery wickets
+#'
+#' @references
+#' \url{http://cricsheet.org/}\cr
+#' \url{https://gigadom.wordpress.com/}
+#' @author
+#' Tinniam V Ganesh
+#' @note
+#' Maintainer: Tinniam V Ganesh \email{tvganesh.85@gmail.com}
+#'
+#' @examples
+#' # Create a data frame of deliveries to wickets from the stored .RData files
+#' jadeja1 <- getDeliveryWickets(team="India",dir="../data",name="Jadeja",save=FALSE)
+#'
+#' # Use this to create a classification tree of deliveries to wickets
+#' bowlerWktsPredict(jadeja1,"RA Jadeja")
+#'
+#' @seealso
+#' \code{\link{bowlerMovingAverage}}\cr
+#' \code{\link{getTeamBowlingDetails}}\cr
+#' \code{\link{bowlerWktsPredict}}\cr
+#' \code{\link{teamBowlerWicketRunsOppnAllMatches}}
+#'
+#' @export
+#'
 getDeliveryWickets <- function(team,dir=".",name,save=FALSE){
 
     a <- paste(dir,"/","*",team,"*",sep="")
-    
+
     # Gather team against all opposition
     fl <- Sys.glob(a)
     deliveryWKts <- NULL
