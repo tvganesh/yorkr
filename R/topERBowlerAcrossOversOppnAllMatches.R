7733fa8284fe9123b66750c2cee8508edb3f3fb1
Tinniam V Ganesh <tvganesh.85@gmail.com>
2021-11-05 18:44:06 +0530

    top ER

--- R/topERBowlerAcrossOversOppnAllMatches.R
+++ R/topERBowlerAcrossOversOppnAllMatches.R
@@ -1,7 +1,7 @@
-########################################################################################
+#######################################################################################
 # Designed and developed by Tinniam V Ganesh
 # Date : 5 Nov 2021
-# Function: topWicketsBowlerAcrossOversOppnAllMatches
+# Function: topERBowlerAcrossOversOppnAllMatches.R
 # This function computes the best ER by bowlers in matches against opposition in powerplay, middle and death overs
 #
 ###########################################################################################
@@ -9,10 +9,10 @@
 #' Compute the best ER by bowlers against team in powerplay, middle and death overs
 #'
 #' @description
-#' This function  computes the highest wickets by bowlers against team in in powerplay, middle and death overs
+#' This function  computes the best ER by bowlers against team in in powerplay, middle and death overs
 #'
 #' @usage
-#' topWicketsBowlerAcrossOversOppnAllMatches(matches,t1,plot=1)
+#' topERBowlerAcrossOversOppnAllMatches.R(matches,t1,plot=1)
 #'
 #' @param match
 #' The dataframe of the match
@@ -40,7 +40,7 @@
 #' \dontrun{
 #'
 #' # Plot tne match worm plot
-#' topWicketsBowlerAcrossOversOppnAllMatches(matches,'England')
+#' topERBowlerAcrossOversOppnAllMatches.R(matches,'England')
 #' }
 #' @seealso
 #' \code{\link{getBatsmanDetails}}\cr
@@ -50,7 +50,7 @@
 #'
 #' @export
 #'
-topWicketsBowlerAcrossOversOppnAllMatches <- function(matches,t1,plot=1) {
+topERBowlerAcrossOversOppnAllMatches <- function(matches,t1,plot=1) {
   team=ball=totalRuns=total=NULL
   ggplotly=NULL
 
@@ -58,33 +58,25 @@ topWicketsBowlerAcrossOversOppnAllMatches <- function(matches,t1,plot=1) {
   a <-filter(matches,team!=t1)
   # Power play
   a1 <- a %>% filter(between(as.numeric(str_extract(ball, "\\d+(\\.\\d+)?$")), 0.1, 5.9))
-  a2 <- select(a1,date,bowler,wicketPlayerOut)
-  a3 <- a2 %>%  group_by(bowler,date) %>% filter(wicketPlayerOut != "nobody") %>% mutate(wickets =n())
-  a4 <- a3 %>% select(date,bowler,wickets) %>% distinct(date,bowler,wickets) %>% group_by(bowler) %>% summarise(wicketsPowerPlay=sum(wickets)) %>%
-             arrange(desc(wicketsPowerPlay))
-
-
+  a2 <- select(a1,team,bowler,date,totalRuns)
+  a3 <- a2 %>% group_by(bowler) %>% summarise(total=sum(totalRuns),count=n(), ERPowerPlay=total/count *6)
+  a4 <- a3 %>% select(bowler,ERPowerPlay) %>% arrange(ERPowerPlay)
 
   # Middle overs
   b1 <- a %>% filter(between(as.numeric(str_extract(ball, "\\d+(\\.\\d+)?$")), 6.1, 15.9))
-  b2 <- select(b1,date,bowler,wicketPlayerOut)
-  b3 <- b2 %>%  group_by(bowler,date) %>% filter(wicketPlayerOut != "nobody") %>% mutate(wickets =n())
-  b4 <- b3 %>% select(date,bowler,wickets) %>% distinct(date,bowler,wickets) %>% group_by(bowler) %>% summarise(wicketsMiddleOvers=sum(wickets)) %>%
-    arrange(desc(wicketsMiddleOvers))
-
-
+  b2 <- select(b1,team,bowler,date,totalRuns)
+  b3 <- b2 %>% group_by(bowler) %>% summarise(total=sum(totalRuns),count=n(), ERMiddleOvers=total/count *6)
+  b4 <- b3 %>% select(bowler,ERMiddleOvers) %>% arrange(ERMiddleOvers)
 
   #Death overs
   c1 <- a %>% filter(between(as.numeric(str_extract(ball, "\\d+(\\.\\d+)?$")), 16.1, 20.0))
-  c2 <- select(c1,date,bowler,wicketPlayerOut)
-  c3 <- c2 %>%  group_by(bowler,date) %>% filter(wicketPlayerOut != "nobody") %>% mutate(wickets =n())
-  c4 <- c3 %>% select(date,bowler,wickets) %>% distinct(date,bowler,wickets) %>% group_by(bowler) %>% summarise(wicketsDeathOvers=sum(wickets)) %>%
-    arrange(desc(wicketsDeathOvers))
+  c2 <- select(c1,team,bowler,date,totalRuns)
+  c3 <- c2 %>% group_by(bowler) %>% summarise(total=sum(totalRuns),count=n(), ERDeathOvers=total/count *6)
+  c4 <- c3 %>% select(bowler,ERDeathOvers) %>% arrange(ERDeathOvers)
 
   val=min(dim(a4)[1],dim(b4)[1],dim(c4)[1])
   m=cbind(a4[1:val,],b4[1:val,],c4[1:val,])
-
   m
 
-
 }
+
